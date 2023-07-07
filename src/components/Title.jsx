fbb8d10f2b4a9c7170228de4f541a1aee8c9d600
mellcosta <m3licosta@gmail.com>
2023-07-07 22:22:05 +0100

    SVG para PNG

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -5,7 +5,7 @@ const Title = (props) => {
         <div className="flex flex-col items-center justify-center gap-0 mb-[100px]">
             <h2 className="text-[60px] font-semibold">{props.title}</h2>
             <Image
-                src="/line.png"
+                src="/line.svg"
                 alt="Line"
                 width={205}
                 height={20}
