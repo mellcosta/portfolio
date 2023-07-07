e7465d546d6aee851aa499c3401790ecd184c473
mellcosta <m3licosta@gmail.com>
2023-07-07 21:51:40 +0100

    Card Links

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 const Cards = (props) => {
     return(
-        <section className="flex-col gap-[20px] w-[500px] h-[350px] bg-gray-900 hover:bg-gray-800 rounded-2xl justify-center items-center px-[10px] py-[20px]">
+        <section className="flex-col gap-[20px] w-[500px] h-[400px] bg-gray-900 hover:bg-gray-800 rounded-2xl justify-center items-center px-[10px] py-[20px]">
             <div className="flex flex-col  items-center justify-center gap-[-50px]">
                 <a href={props.url} target="_blank">
                     <Image
@@ -10,7 +10,7 @@ const Cards = (props) => {
                         alt="Image"
                         width={500}
                         height={300}
-                        className="rounded-tl-2xl rounded-tr-2xl h-[250px] max-h-[310px]"
+                        className="rounded-tl-2xl rounded-tr-2xl h-[300px] max-h-[310px]"
                     />
                 </a>
                 <Image
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -9,12 +9,23 @@ export default function Projects() {
             <Title title="Projectos"/>
             <div className="flex flex-wrap items-center justify-center gap-[50px]">
                 <Card image="/countdown.png" legend="Countdown" 
-                    url="https://mellcosta.github.io/desafiosRocketseat/intermediario/countdown/countdown.html"/>
-                <Card image="/loginPage.png" legend="Login Page" />
-                <Card image="/spotify.png" legend="Spotify Clone" />
-                <Card image="/portfolio.png" legend="Rocketseat Portfolio" />
-                <Card image="/ignite-lds.png" legend="Design System" />
-                <Card image="/imp-jfr.png" legend="Website Institucional" height="200px" />
+                    url="https://mellcosta.github.io/desafiosRocketseat/intermediario/countdown/countdown.html"
+                />
+                <Card image="/loginPage.png" legend="Login Page" 
+                    url="https://mellcosta.github.io/desafiosRocketseat/intermediario/login/login.html"
+                />
+                <Card image="/spotify.png" legend="Spotify Clone" 
+                    url="https://spotify-cyan-chi.vercel.app/"
+                />
+                <Card image="/portfolio.png" legend="Rocketseat Portfolio" 
+                    url="https://mellcosta.github.io/desafiosRocketseat/iniciante/portfolio/portfolio.html"
+                />
+                <Card image="/ignite-lds.png" legend="Design System" 
+                    url="https://mellcosta.github.io/ignite_lab_design_system/?path=/story/components-button--default"
+                />
+                <Card image="/imp-jfr.png" legend="Website Institucional"
+                    url="https://mellcosta.github.io/website/src/home.html"
+                />
             </div>
         </div>
     )
