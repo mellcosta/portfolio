0145f3bb90af6a14470bf7645fa83f7ce4e0d30d
mellcosta <m3licosta@gmail.com>
2023-07-14 11:49:49 +0100

    React Scroller Pendente

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,26 +3,25 @@ import {Link as ScrollLink } from "react-scroll";
 export default function Nav() {
     return (
         <nav className=" menu-nav flex justify-center items-center gap-[30px]">
-            <ScrollLink activeClass="active" to="about" spy={true} smooth={true} offset={50} duration={500}
-              
+            <ScrollLink to="about"
               className="font-normal text-[30px] cursor-pointer"
             >
               Sobre
             </ScrollLink>
             <ScrollLink
-              href="#techs"
+              to="techs"
               className="font-normal text-[30px] cursor-pointer"
             >
               Tecnologias
             </ScrollLink>
             <ScrollLink
-              href="#projects"
+              to="projects"
               className="font-normal text-[30px] cursor-pointer"
             >
               Projectos
             </ScrollLink>
             <ScrollLink
-              href="#contact"
+              to="contact"
               className="font-normal text-[30px] cursor-pointer"
             >
               Contacto
